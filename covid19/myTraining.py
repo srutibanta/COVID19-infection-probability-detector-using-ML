9318364f7d6eb053d024e9cf82adc0eb9ae525f0
srutibanta <56752386+srutibanta@users.noreply.github.com>
2020-11-15 12:18:44 +0530

    Add files via upload

---
+++ covid19/myTraining.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+def data_split(data,ratio):
+    np.random.seed(42)
+    shuffled=np.random.permutation(len(data))
+    test_set_size=int(len(data)*ratio)
+    test_indices=shuffled[:test_set_size]
+    train_indices=shuffled[test_set_size:]
+    return data.iloc[train_indices],data.iloc[test_indices]
+if __name__ == "__main__":
+    df=pd.read_csv('data.csv')
+    train,test=data_split(df,0.2)
+    X_train = train[['fever','bodypain','age','runnynose','diffbreath']].to_numpy()
+    X_test = test[['fever','bodypain','age','runnynose','diffbreath']].to_numpy()
+    Y_train = train[['infectionprob']].to_numpy().reshape(2800,)
+    Y_test = test[['infectionprob']].to_numpy().reshape(699,)
+    clf=LogisticRegression()
+    clf.fit(X_train,Y_train)
+    file=open('model.pkl', 'wb')
+    pickle.dump(clf,file)
+    file.close()
+    
